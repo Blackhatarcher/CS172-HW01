f1513030508d7add15122c4bb1afcd50d0236b06
christianwilliams20 <christianwilliams20@LAB-SCI-214-07>
2016-10-04 15:11:13 -0700

    fixed all the stuff very happy right now

--- Learning/Learning/Dice.cpp
+++ Learning/Learning/Dice.cpp
@@ -11,4 +11,9 @@ int Dice::Roll()
 {
 	return sides;
 }
-
+int Dice::GetRolls() {
+	return 0;
+}
+int Dice::GetSides() {
+	return 0;
+}
--- Learning/Learning/Dice.h
+++ Learning/Learning/Dice.h
@@ -1,12 +1,13 @@
 #pragma once
-//#ifndef Dice_h
-//#define Dice_h
 
 class Dice
 {
 public:
 	Dice(int side);
-	int sides;
 	int Roll();
+	int GetRolls();
+	int GetSides();
+private:
+	int sides;
 };
 
